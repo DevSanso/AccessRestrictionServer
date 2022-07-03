a557f52bc8f9a187f439de8263d4dc9d73b4ad07
Sunken Ahn <devsanso@outlook.com>
2022-07-03 16:44:55 +0900

    프로젝트 추가 : 사용자 요청 처리 및 접근 확인 서버

---
+++ cmd/server/gateway/src/main/kotlin/GatewayApplication.kt
@@ -0,0 +1,12 @@
+package com.github.DevSanso.accessRestrictionGateway.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
