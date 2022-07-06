cb5cee467ec25f241d05fa4a77cde85837a06625
Sunken Ahn <devsanso@outlook.com>
2022-07-06 11:36:40 +0900

    컨테이너 테이블, 사용할 프로세서 정보 저장

--- cmd/server/gateway/src/main/kotlin/GatewayApplication.kt
+++ cmd/server/gateway/src/main/kotlin/GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.github.DevSanso.accessRestrictionGateway.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 
+@ComponentScan(basePackages = ["com.github.DevSanso.accessRestrictionGateway"])
 @SpringBootApplication
 class GatewayApplication
 
