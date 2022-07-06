7dea172baea23669c638d0d5c7145d3483a5ba06
Sunken Ahn <devsanso@outlook.com>
2022-07-06 18:01:52 +0900

    proto 데이터 인코딩, 디코딩 소스코드

---
+++ cmd/containers/url_mapping/parser/msa_header.go
@@ -0,0 +1,17 @@
+package parser
+
+import parser "google.golang.org/protobuf/proto"
+import  "core/proto"
+
+
+
+func EncodeHeader(data []byte) (proto.MSAHeader,error) {
+	var buf = proto.MSAHeader{}
+	err := parser.Unmarshal(data,&buf)
+	return buf,err
+}
+
+func DecodeHeader(header *proto.MSAHeader) ([]byte,error) {
+	return parser.Marshal(header)
+}
+
