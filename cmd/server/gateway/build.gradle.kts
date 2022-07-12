7ccadc29534ed70651a2186ff814fad6eb466b44
Sunken Ahn <devsanso@outlook.com>
2022-07-13 01:58:29 +0900

    test 모듈 추가

--- cmd/server/gateway/build.gradle.kts
+++ cmd/server/gateway/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.2-SNAPSHOT"
+	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	id("idea")
 }
 
 group = "com.github.DevSanso.accessRestrictionGateway"
@@ -17,13 +18,18 @@ repositories {
 }
 
 dependencies {
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("junit:junit:4.13.2")
+	testImplementation("com.h2database:h2:2.1.214")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.apache.kafka:kafka-streams")
+	implementation("org.apache.kafka:kafka-clients:3.2.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation(project(":dbc"))
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.apache.kafka:kafka-clients:3.2.0")
+	//implementation(project(":dbc"))
+
 }
 
 tasks.withType<KotlinCompile> {
