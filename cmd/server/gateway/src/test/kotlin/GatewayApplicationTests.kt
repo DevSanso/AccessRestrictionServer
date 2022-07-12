946dbddb6949ec9d8f01fa3ed514602939ca3ef2
Sunken Ahn <devsanso@outlook.com>
2022-07-12 19:42:31 +0900

    어플리케이션 테스트  클래스

--- cmd/server/gateway/src/test/kotlin/GatewayApplicationTests.kt
+++ cmd/server/gateway/src/test/kotlin/GatewayApplicationTests.kt
@@ -1,13 +1,12 @@
 package com.github.DevSanso.accessRestrictionGateway.gateway
 
+import com.github.DevSanso.accessRestrictionGateway.gateway.configure.KafkaConfigure
 import org.junit.jupiter.api.Test
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootTest
-class GatewayApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+@PropertySource("classpath:application.properties")
+@EnableConfigurationProperties(KafkaConfigure::class)
+class GatewayApplicationTests {}
